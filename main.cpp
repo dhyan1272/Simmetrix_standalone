a0cc8038121ebb2d9d55b8dec7b36fdbfe04ba87
nathd2 <nathd@rpi.edu>
2024-10-21 11:57:35 -0400

    Some more fucntionality

--- main.cpp
+++ main.cpp
@@ -11,12 +11,34 @@ int main(){
   
   pNativeModel nm = NULL;
   pProgress p = NULL;
-  pGModel g = GM_load("/lore/nathd2/TOMMS_16.0/ITER_He/tomms_io/simModel_result.smd", nm, p);
-  
-  GM_release(g);
-  SimModel_stop();
-  
-  std::cout<<"Done"<<std::endl;
+  pGModel g = GM_load("/users/nathd2/2BOX/Box_s1.smd", nm, p);
+  pMesh sm =  M_load("/users/nathd2/2BOX/Box_1-case1.sms", g, p);
 
+  //Loop over elements 
+  RIter regions = M_regionIter(sm);
+  pRegion rgn;
+  int count = 0;
+  while ((rgn = (pRegion) RIter_next(regions))) {
+    count ++;
+
+  }
+  RIter_delete(regions);
+  std::cout<<"Count regions: "<<count<<std::endl;
+
+  //Loop over vertices
+  VIter vertices = M_vertexIter(sm);
+  pVertex vtx;
+  while ((vtx = (pVertex) VIter_next(vertices))) {
+     double xyz[3];
+     V_coord(vtx,xyz);
+  }
+  VIter_delete(vertices);
+
+
+  M_release(sm);
+  GM_release(g);
+
+  SimModel_stop();
+  std::cout<<"Done"<<std::endl;
   return 0;
 }
