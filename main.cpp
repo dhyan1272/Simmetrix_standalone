248b2f0751dceacb328be9421619bb21f7bec273
nathd2 <nathd@rpi.edu>
2024-10-29 11:09:49 -0400

    Some more Simmetrix APIs

--- main.cpp
+++ main.cpp
@@ -3,6 +3,55 @@
 #include "SimUtil.h"
 #include "SimDiscrete.h"
 
+void writeVTK(const std::string &filename, const std::vector<std::array<double, 3>> &points, const std::vector<double> &fieldValues) {
+  //Open the output file
+  std::ofstream vtkFile(filename);
+  if (!vtkFile.is_open()) {
+    std::cerr << "Error opening file: " << filename << std::endl;
+    return;
+  }
+  // VTK file header
+  vtkFile << "# vtk DataFile Version 3.0\n";
+  vtkFile << "Point data with scalar values\n";
+  vtkFile << "ASCII\n";
+  vtkFile << "DATASET POLYDATA\n";
+  // Write the points
+  vtkFile << "POINTS " << points.size() << " double\n";
+  for (const auto &point : points) {
+    vtkFile << point[0] << " " << point[1] << " " << point[2] << "\n";
+  }
+  // Write the point scalar values
+  vtkFile << "POINT_DATA " << fieldValues.size() << "\n";
+  vtkFile << "FIELD FieldData 1\n";  // FIELD section starts here
+  vtkFile << "FieldValues 1 " << fieldValues.size() << " double\n";  // Define the field with name "FieldValues"
+  for (const auto &value : fieldValues) {
+    vtkFile << value << "\n";
+  }
+  // Close the file
+  vtkFile.close();
+  std::cout << "VTK file written: " << filename << std::endl;
+}
+
+
+void writetoTxt(const std::string& filename, const std::vector<std::array<double, 3>>& points, 
+  const std::vector<double>& fields) {
+  
+  std::ofstream outfile(filename);
+  if (!outfile) {
+     std::cerr << "Error opening file: " << filename << std::endl;
+     return;
+  }
+  outfile << "X Y Z Field\n";  // Optional header line
+  for (size_t i = 0; i < points.size(); ++i) {
+    const auto& point = points[i];
+    double field = fields[i];
+    outfile << point[0] << " " << point[1] << " " << point[2] << " " << field << "\n";
+  }
+  outfile.close();
+  std::cout << "Data written to " << filename << std::endl;
+
+}
+
 int main(){
   printf("Compiled & running C++ code with Simmetrix Libs using CMake \n");
   
@@ -11,8 +60,31 @@ int main(){
   
   pNativeModel nm = NULL;
   pProgress p = NULL;
-  pGModel g = GM_load("/users/nathd2/2BOX/Box_s1.smd", nm, p);
+  pGModel g = GM_load("/users/nathd2/2BOX/Box_1_sim.smd", nm, p);
   pMesh sm =  M_load("/users/nathd2/2BOX/Box_1-case1.sms", g, p);
+
+  //Box_dimensions
+  double x_lower = -0.5;
+  double x_upper = 0.5;
+  double height = 0.25;
+
+  //Random number
+  int seed=100;
+  double min_value=-0.35;
+  double max_value=0.35;
+  std::mt19937 generator(seed);
+  std::uniform_real_distribution<double> distribution(min_value, max_value);
+  
+  int n_samples=1200;
+  for (int smp=0; smp<n_samples; smp++) { double x_random = distribution(generator);
+  
+  std::cout<<"Sample: "<< smp <<"  X:"<<x_random<<std::endl; 
+  double m2 = (height)/(x_upper-x_random);
+  double m1 = (height)/(x_lower-x_random);
+
+  //Array
+  std::vector<std::array<double, 3>> points;
+  std::vector<double> fields;
 
   //Loop over elements 
   RIter regions = M_regionIter(sm);
@@ -25,16 +97,69 @@ int main(){
     int x = EN_coord(rgn, xyz);
     EN_centroid	(rgn, cent);
     
-    //Debugging
-    if (count==0){
-      for (int i=0; i<4; i++)
-        printf("Vertex coords %d %.15e %.15e %.15e \n", i, xyz[i][0], xyz[i][1], xyz[i][2]);
-      printf("Centroid %.15e %.15e %.15e \n", cent[0], cent[1], cent[2]);
+    double field_local=0;
+    if(cent[0]<x_random){
+      double y1_upper = -0.5*height + m1*(cent[0]-x_random);
+      if(cent[2]<y1_upper)
+        field_local=1;
+      else 
+	field_local=1.2;
     }
+    else{
+      double y2_upper = -0.5*height + m2*(cent[0]-x_random);
+      if(cent[2]<y2_upper)
+        field_local=1.3;
+      else 
+        field_local=1.2;
+    }
+	
+    points.push_back({cent[0], cent[1], cent[2]});
+    fields.push_back(field_local);
     count ++;
   }
   RIter_delete(regions);
-  std::cout<<"Count regions: "<<count<<std::endl;
+  //std::cout<<"Count regions: "<<count<<std::endl;
+
+  /*
+  //Loop over faces
+  count=0;
+  FIter faces = M_faceIter(sm);
+  pFace thisFace;
+  while ((thisFace = (pFace) FIter_next(faces))) {
+    if(F_whatInType(thisFace) == 2){
+      pGFace nonManFace = (pGFace) F_whatIn(thisFace);
+      int faceTag = GEN_tag(nonManFace);
+      //Temporary
+      if(faceTag==6){
+        double xyz[3][3];
+        double cent[3];
+        int x = EN_coord(thisFace, xyz);
+        EN_centroid(thisFace, cent);
+        //Fields on face centroids
+        double field_local=0;
+        if(cent[0]<x_random){
+          double y1_upper = -0.5*height + m1*(cent[0]-x_random);
+        if(cent[2]<y1_upper)
+          field_local=1;
+        else 
+	  field_local=1.2;
+        }
+        else{
+          double y2_upper = -0.5*height + m2*(cent[0]-x_random);
+          if(cent[2]<y2_upper)
+            field_local=1.3;
+          else 
+            field_local=1.2;
+        }
+        points.push_back({cent[0], cent[1], cent[2]});
+        fields.push_back(field_local);
+	
+      }//tag
+    }
+    count++;
+  }
+  FIter_delete(faces);
+  std::cout<<"Count Faces: "<<count<<std::endl;
 
   //Loop over vertices
   count=0;
@@ -47,6 +172,12 @@ int main(){
   }
   VIter_delete(vertices);
   std::cout<<"Count Vertices: "<<count<<std::endl;
+  // Till here
+  */
+  std::string file_name="centroid_field" + std::to_string(smp) + ".txt";
+  writetoTxt(file_name, points, fields); }
+  
+  //writeVTK("dynamic_points_with_values.vtk", points, fields);
 
   M_release(sm);
   GM_release(g);
